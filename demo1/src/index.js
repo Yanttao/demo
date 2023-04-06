82c72a1a9d36403a1b2e2eaad7d884ce47e0731c
yantao <853110993@qq.com>
2023-04-06 08:48:08 +0800

    结束webpack

---
+++ demo1/src/index.js
@@ -0,0 +1,13 @@
+// import "./style/index.css"
+// import An from "./assets/3.jpg"
+
+document.body.insertAdjacentHTML("beforeend","<h1>今天天气真不错，天空晴空万里！</h1>")
+// document.body.insertAdjacentHTML("beforeend",`<img src="${An}">`)
+
+document.body.onclick = ()=>{
+    alert("点我干嘛")
+}
+
+
+
+
--- demo1/webpack.config.js
+++ demo1/webpack.config.js
@@ -1,8 +1,49 @@
+const path = require("path")
+const HTMLplugin = require("html-webpack-plugin")
 module.exports = {
     mode:"production",
     // entry:["./src/a.js" , "./src/b.js"]
-    entry:{
-        hello:"./src/a.js",
-        b:"./src/b.js"
-    }
+    // entry:{
+    //     hello:"./src/a.js",
+    //     b:"./src/b.js"
+    // }
+
+    // entry:"./src/a.js",
+
+    
+    output:{
+        path:path.resolve(__dirname , "dist"),
+        filename:"main.js",
+        clean:true
+    },
+
+
+    module:{
+        rules:[
+            {
+                test:/\.css$/i,
+                use:["style-loader" , "css-loader"]
+            },{
+                test:/\.jpg$/i,
+                type:"asset/resource"
+            },{
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+            }
+        ]
+    },
+
+    plugins:[
+        new HTMLplugin({
+            template:"./src/index.html"
+        }) 
+    ],
+
+    devtool:"inline-source-map"
 }
